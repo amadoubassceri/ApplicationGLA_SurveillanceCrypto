a0b816ff5db679bd422c16222a5d8863d70bcc75
ADMIN <narutokonoya1@gmail.com>
2024-12-06 15:41:00 +0100

    backend : ajout de l'api rest pour pouvoir le lier avec la partie frontend

--- src/main/java/com/crypto/repository/CryptoPriceRepository.java
+++ src/main/java/com/crypto/repository/CryptoPriceRepository.java
@@ -2,7 +2,8 @@ package com.crypto.repository;
 
 import com.crypto.model.CryptoPrice;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import java.util.List;
 public interface CryptoPriceRepository extends MongoRepository<CryptoPrice, String> {
-    CryptoPrice findBySymbol(String symbol);
+    //CryptoPrice findBySymbol(String symbol);
+    List<CryptoPrice> findTop10ByOrderByIdAsc(); // Cette méthode récupère les 10 premiers éléments triés par ID
 }
